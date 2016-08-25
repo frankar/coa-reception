01b2475ef287b76c5cd2ca4d73cf6a6ca79263e8
fc <franco@carinato.net>
2016-08-25 19:28:05 +0200

    Modifica al footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		</div>
 		<div id="footer">
 			Ver. <?php echo $current_ver ?>. Sviluppo iniziato nel COA Veneto a Ferrara - dal 15 al 20 Luglio 2012 - Per assistenza su questo applicativo contattare <a href="mailto:franco@carinato.net">Franco Carinato</a><br />
-			Sviluppato dal V.C. Franco Carinato, in collaborazione con il V.C.T.I. Daniele Romano e il V.C.T.I. Giuseppe Maltese
+			Sviluppato dal C.S. Franco Carinato del Comando di Treviso, in collaborazione con il V.C.T.I. Daniele Romano e il V.C.T.I. Giuseppe Maltese
 		</div>
 	</div>
 </body>
